4d6a7a681c9680a72f76c400ea86c93e2dfb51d6
Remi Caput <git@mirari.fr>
2014-05-19 11:51:54 +0200

    Remove explicit layout on value, update nuspec file.

--- Verse/src/Schemas/JSON/Value.cs
+++ Verse/src/Schemas/JSON/Value.cs
@@ -3,19 +3,14 @@ using System.Runtime.InteropServices;
 
 namespace Verse.Schemas.JSON
 {
-	[StructLayout (LayoutKind.Explicit)]
 	public struct Value
 	{
-		[FieldOffset (sizeof (Content))]
 		public bool		Boolean;
 
-		[FieldOffset (sizeof (Content))]
 		public double	Number;
 
-		[FieldOffset (sizeof (Content) + sizeof (double))]
 		public string	String;
 
-		[FieldOffset (0)]
 		public Content	Type;
 	}
 }
