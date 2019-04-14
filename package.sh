bf0b8fef589b7f3f1ccb799bf54123596a2ad144
Remi Caput <git@mirari.fr>
2019-04-15 00:03:56 +0200

    Add `package.sh` packaging script.

---
+++ package.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -e
+
+base="$(dirname "$0")"
+root=nbrowse
+
+# Retreive latest version from current HEAD
+version="$(git --work-tree "$base" tag --points-at HEAD)"
+
+if [ -z "$version" ]; then
+	echo >&2 "error: current HEAD doesn't point to a tag"
+	exit 1
+fi
+
+dotnet pack -c Release "$base/Verse/Verse.csproj"
