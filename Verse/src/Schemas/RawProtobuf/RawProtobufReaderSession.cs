87137a36c8e900727f25a93d885b2eda4da774a4
Remi Caput <git@mirari.fr>
2019-04-17 23:10:47 +0200

    Do not expose Protobuf-net internals to raw protobuf reader.

--- Verse/src/Schemas/RawProtobuf/RawProtobufReaderSession.cs
+++ Verse/src/Schemas/RawProtobuf/RawProtobufReaderSession.cs
@@ -14,52 +14,37 @@ namespace Verse.Schemas.RawProtobuf
 			return (int index, out TElement element) =>
 			{
 				// Read next field header if required so we know whether it's still part of the same array or not
-				if (!state.FieldType.HasValue)
-					state.ReadHeader();
+				state.ReadHeader();
+
+				// Read field and continue enumeration if we're still reading elements sharing the same field index
+				if (fieldIndex == state.FieldIndex)
+					return callback(this, state, out element) ? BrowserState.Continue : BrowserState.Failure;
 
 				// Different field index (or end of stream) was met, stop enumeration
-				if (fieldIndex != state.FieldIndex)
-				{
-					element = default;
+				element = default;
 
-					return BrowserState.Success;
-				}
-
-				// Read field and continue enumeration
-				if (!callback(this, state, out element))
-					return BrowserState.Failure;
-
-				return BrowserState.Continue;
+				return BrowserState.Success;
 			};
 		}
 
 		public bool ReadToObject<TObject>(RawProtobufReaderState state,
 			ILookup<int, ReaderSetter<RawProtobufReaderState, RawProtobufValue, TObject>> fields, ref TObject target)
 		{
-			// Complex objects are expected to be at top-level (no parent field type) or contained within string type
-			if (state.FieldType.GetValueOrDefault(WireType.String) != WireType.String)
+			if (!state.ObjectBegin(out var subItem))
 			{
 				RawProtobufReaderSession.Skip(state);
 
 				return true;
 			}
 
-			var subItem = state.FieldType.HasValue ? ProtoReader.StartSubItem(state.Reader) : (SubItemToken?) null;
-
-			state.ClearHeader();
-
 			while (true)
 			{
-				if (!state.FieldType.HasValue)
-					state.ReadHeader();
+				state.ReadHeader();
 
 				// Stop reading complex object when no more field can be read
 				if (state.FieldIndex <= 0)
 				{
-					state.ClearHeader();
-
-					if (subItem.HasValue)
-						ProtoReader.EndSubItem(subItem.Value, state.Reader);
+					state.ObjectEnd(subItem);
 
 					return true;
 				}
@@ -81,39 +66,7 @@ namespace Verse.Schemas.RawProtobuf
 
 		public bool ReadToValue(RawProtobufReaderState state, out RawProtobufValue value)
 		{
-			var fieldType = state.FieldType.GetValueOrDefault();
-
-			state.ClearHeader();
-
-			switch (fieldType)
-			{
-				case WireType.Fixed32:
-					value = new RawProtobufValue(state.Reader.ReadInt32(), RawProtobufStorage.Fixed32);
-
-					return true;
-
-				case WireType.Fixed64:
-					value = new RawProtobufValue(state.Reader.ReadInt64(), RawProtobufStorage.Fixed64);
-
-					return true;
-
-				case WireType.String:
-					value = new RawProtobufValue(state.Reader.ReadString(), RawProtobufStorage.String);
-
-					return true;
-
-				case WireType.Variant:
-					value = new RawProtobufValue(state.Reader.ReadInt64(), RawProtobufStorage.Variant);
-
-					return true;
-
-				default:
-					state.Error($"unsupported wire type {state.FieldType} for field {state.FieldIndex}");
-
-					value = default;
-
-					return false;
-			}
+			return state.TryReadValue(out value);
 		}
 
 		public bool Start(Stream stream, DecodeError error, out RawProtobufReaderState state)
@@ -129,8 +82,7 @@ namespace Verse.Schemas.RawProtobuf
 
 		private static bool Skip(RawProtobufReaderState state)
 		{
-			state.Reader.SkipField();
-			state.ClearHeader();
+			state.SkipField();
 
 			return true;
 		}
--- Verse/src/Schemas/RawProtobuf/RawProtobufReaderState.cs
+++ Verse/src/Schemas/RawProtobuf/RawProtobufReaderState.cs
@@ -6,38 +6,109 @@ namespace Verse.Schemas.RawProtobuf
 {
 	internal class RawProtobufReaderState
 	{
-		public readonly ProtoReader Reader;
-
 		public int FieldIndex;
 
-		public WireType? FieldType;
-
 		private readonly DecodeError error;
+
+		private WireType? fieldType;
+
+		private readonly ProtoReader reader;
 
 		public RawProtobufReaderState(Stream stream, DecodeError error)
 		{
 			this.error = error;
 
 			this.FieldIndex = 0;
-			this.FieldType = null;
-			this.Reader = new ProtoReader(stream, TypeModel.Create(), null);
-		}
-
-		public void ClearHeader()
-		{
-			this.FieldIndex = 0;
-			this.FieldType = null;
-		}
-
-		public void ReadHeader()
-		{
-			this.FieldIndex = this.Reader.ReadFieldHeader();
-			this.FieldType = this.Reader.WireType;
+			this.fieldType = null;
+			this.reader = new ProtoReader(stream, TypeModel.Create(), null);
 		}
 
 		public void Error(string message)
 		{
-			this.error(this.Reader.Position, message);
+			this.error(this.reader.Position, message);
+		}
+
+		public bool ObjectBegin(out SubItemToken? subItem)
+		{
+			// Complex objects are expected to be at top-level (no parent field type) or contained within string type
+			if (this.fieldType.GetValueOrDefault(WireType.String) != WireType.String)
+			{
+				subItem = default;
+
+				return false;
+			}
+
+			subItem = this.fieldType.HasValue ? ProtoReader.StartSubItem(this.reader) : (SubItemToken?) null;
+
+			this.ClearHeader();
+
+			return true;
+		}
+
+		public void ObjectEnd(SubItemToken? subItem)
+		{
+			this.ClearHeader();
+
+			if (subItem.HasValue)
+				ProtoReader.EndSubItem(subItem.Value, this.reader);
+		}
+
+		public void ReadHeader()
+		{
+			if (this.fieldType.HasValue)
+				return;
+
+			this.FieldIndex = this.reader.ReadFieldHeader();
+			this.fieldType = this.reader.WireType;
+		}
+
+		public bool TryReadValue(out RawProtobufValue value)
+		{
+			var valueFieldType = this.fieldType.GetValueOrDefault();
+
+			this.ClearHeader();
+
+			switch (valueFieldType)
+			{
+				case WireType.Fixed32:
+					value = new RawProtobufValue(this.reader.ReadInt32(), RawProtobufStorage.Fixed32);
+
+					return true;
+
+				case WireType.Fixed64:
+					value = new RawProtobufValue(this.reader.ReadInt64(), RawProtobufStorage.Fixed64);
+
+					return true;
+
+				case WireType.String:
+					value = new RawProtobufValue(this.reader.ReadString(), RawProtobufStorage.String);
+
+					return true;
+
+				case WireType.Variant:
+					value = new RawProtobufValue(this.reader.ReadInt64(), RawProtobufStorage.Variant);
+
+					return true;
+
+				default:
+					this.Error($"unsupported wire type {this.fieldType} for field {this.FieldIndex}");
+
+					value = default;
+
+					return false;
+			}
+		}
+
+		public void SkipField()
+		{
+			this.reader.SkipField();
+			this.ClearHeader();
+		}
+
+		private void ClearHeader()
+		{
+			this.FieldIndex = 0;
+			this.fieldType = null;
 		}
 	}
 }
