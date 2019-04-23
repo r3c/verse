c01dd624ca5658a3c3e8aa4aed99b2b7a8a8c9ff
Xavier Solau <x.solau@criteo.com>
2019-04-23 17:47:17 +0200

    Add access to the RawProtobufValue converters (from/to).

--- Verse/src/Schemas/RawProtobuf/RawProtobufValue.cs
+++ Verse/src/Schemas/RawProtobuf/RawProtobufValue.cs
@@ -22,5 +22,45 @@ namespace Verse.Schemas.RawProtobuf
             this.Storage = storage;
             this.String = value;
         }
-	}
+
+	    private static readonly RawProtobufEncoderConverter converterFrom = new RawProtobufEncoderConverter();
+	    private static readonly RawProtobufDecoderConverter converterTo = new RawProtobufDecoderConverter();
+
+	    public static RawProtobufValue FromLong(long value)
+	    {
+	        return converterFrom.Get<long>()(value);
+	    }
+	    public static RawProtobufValue FromFloat(float value)
+	    {
+	        return converterFrom.Get<float>()(value);
+	    }
+	    public static RawProtobufValue FromDouble(double value)
+	    {
+	        return converterFrom.Get<double>()(value);
+	    }
+	    public static RawProtobufValue FromString(string value)
+	    {
+	        return converterFrom.Get<string>()(value);
+	    }
+
+	    public static long ToLong(RawProtobufValue value)
+	    {
+	        return converterTo.Get<long>()(value);
+	    }
+
+	    public static double ToDouble(RawProtobufValue value)
+	    {
+	        return converterTo.Get<double>()(value);
+	    }
+
+	    public static float ToFloat(RawProtobufValue value)
+	    {
+	        return converterTo.Get<float>()(value);
+	    }
+
+	    public static string ToString(RawProtobufValue value)
+	    {
+	        return converterTo.Get<string>()(value);
+	    }
+    }
 }
