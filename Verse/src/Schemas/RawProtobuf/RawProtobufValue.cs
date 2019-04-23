f3525912086f62b1a139648d961826ec203a51ea
Remi Caput <r.caput@criteo.com>
2019-04-23 18:35:40 +0200

    Style change on `RawProtobufValue` class.

--- Verse/src/Schemas/RawProtobuf/RawProtobufValue.cs
+++ Verse/src/Schemas/RawProtobuf/RawProtobufValue.cs
@@ -3,64 +3,67 @@ namespace Verse.Schemas.RawProtobuf
 {
 	public readonly struct RawProtobufValue
 	{
-        public readonly long Number;
+		private static readonly RawProtobufEncoderConverter ConverterFrom = new RawProtobufEncoderConverter();
+		private static readonly RawProtobufDecoderConverter ConverterTo = new RawProtobufDecoderConverter();
 
-        public readonly RawProtobufStorage Storage;
+		public readonly long Number;
 
-        public readonly string String;
+		public readonly RawProtobufStorage Storage;
 
-        public RawProtobufValue(long number, RawProtobufStorage storage)
-        {
-            this.Number = number;
-            this.Storage = storage;
-            this.String = default;
-        }
+		public readonly string String;
 
-        public RawProtobufValue(string value, RawProtobufStorage storage)
-        {
-            this.Number = default;
-            this.Storage = storage;
-            this.String = value;
-        }
+		public RawProtobufValue(long number, RawProtobufStorage storage)
+		{
+			this.Number = number;
+			this.Storage = storage;
+			this.String = default;
+		}
 
-	    private static readonly RawProtobufEncoderConverter converterFrom = new RawProtobufEncoderConverter();
-	    private static readonly RawProtobufDecoderConverter converterTo = new RawProtobufDecoderConverter();
+		public RawProtobufValue(string value, RawProtobufStorage storage)
+		{
+			this.Number = default;
+			this.Storage = storage;
+			this.String = value;
+		}
 
-	    public static RawProtobufValue FromLong(long value)
-	    {
-	        return converterFrom.Get<long>()(value);
-	    }
-	    public static RawProtobufValue FromFloat(float value)
-	    {
-	        return converterFrom.Get<float>()(value);
-	    }
-	    public static RawProtobufValue FromDouble(double value)
-	    {
-	        return converterFrom.Get<double>()(value);
-	    }
-	    public static RawProtobufValue FromString(string value)
-	    {
-	        return converterFrom.Get<string>()(value);
-	    }
+		public static RawProtobufValue FromLong(long value)
+		{
+			return RawProtobufValue.ConverterFrom.Get<long>()(value);
+		}
 
-	    public static long ToLong(RawProtobufValue value)
-	    {
-	        return converterTo.Get<long>()(value);
-	    }
+		public static RawProtobufValue FromFloat(float value)
+		{
+			return RawProtobufValue.ConverterFrom.Get<float>()(value);
+		}
 
-	    public static double ToDouble(RawProtobufValue value)
-	    {
-	        return converterTo.Get<double>()(value);
-	    }
+		public static RawProtobufValue FromDouble(double value)
+		{
+			return RawProtobufValue.ConverterFrom.Get<double>()(value);
+		}
 
-	    public static float ToFloat(RawProtobufValue value)
-	    {
-	        return converterTo.Get<float>()(value);
-	    }
+		public static RawProtobufValue FromString(string value)
+		{
+			return RawProtobufValue.ConverterFrom.Get<string>()(value);
+		}
 
-	    public static string ToString(RawProtobufValue value)
-	    {
-	        return converterTo.Get<string>()(value);
-	    }
-    }
+		public static long ToLong(RawProtobufValue value)
+		{
+			return RawProtobufValue.ConverterTo.Get<long>()(value);
+		}
+
+		public static double ToDouble(RawProtobufValue value)
+		{
+			return RawProtobufValue.ConverterTo.Get<double>()(value);
+		}
+
+		public static float ToFloat(RawProtobufValue value)
+		{
+			return RawProtobufValue.ConverterTo.Get<float>()(value);
+		}
+
+		public static string ToString(RawProtobufValue value)
+		{
+			return RawProtobufValue.ConverterTo.Get<string>()(value);
+		}
+	}
 }
