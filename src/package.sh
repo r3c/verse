f74f39f5732d3c6b62d9ce0ffa90f7243edb03d6
Remi Caput <v.mirari.fr+git@mirari.fr>
2023-01-22 23:57:58 +0100

    Move source files to src/ directory.

---
+++ src/package.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -e
+
+base="$(dirname "$0")"
+
+# Retrieve latest version from current HEAD
+version="$(git --work-tree "$base" tag --points-at HEAD)"
+
+if [ -z "$version" ]; then
+	echo >&2 "warning: current HEAD doesn't point to a tag"
+fi
+
+dotnet pack --include-symbols -c Release "$base"/Verse
