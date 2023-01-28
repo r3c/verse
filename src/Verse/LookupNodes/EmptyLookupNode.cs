72cd89d043460f24b5629dbbb2b89049aeaab1f0
Remi Caput <v.mirari.fr+git@mirari.fr>
2023-01-28 14:49:10 +0100

    Remove null-related compilation warnings in `ILookupNode`.

--- src/Verse/LookupNodes/EmptyLookupNode.cs
+++ src/Verse/LookupNodes/EmptyLookupNode.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Verse.LookupNodes;
 
 internal class EmptyLookupNode<TKey, TValue> : ILookupNode<TKey, TValue>
@@ -5,7 +7,7 @@ internal class EmptyLookupNode<TKey, TValue> : ILookupNode<TKey, TValue>
     public static readonly EmptyLookupNode<TKey, TValue> Instance = new();
 
     public bool HasValue => false;
-    public TValue? Value => default;
+    public TValue Value => throw new InvalidOperationException();
 
     public ILookupNode<TKey, TValue> Follow(TKey key)
     {
